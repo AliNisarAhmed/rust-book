5334dee6774a7e42b916c8200062f22342462a1a
Ali Nisar Ahmed <ali.nisar87@gmail.com>
2022-03-09 21:55:36 -0700

    ch6: memory mgmt

--- rust-in-action/ch6/pointers/src/main.rs
+++ rust-in-action/ch6/pointers/src/main.rs
@@ -1,33 +1,38 @@
-use std::mem::size_of;
+use std::borrow::Cow;
+use std::ffi::CStr;
+use std::os::raw::c_char;
 
 static B: [u8; 10] = [99, 97, 114, 114, 121, 116, 111, 119, 101, 108];
 static C: [u8; 11] = [116, 104, 97, 110, 107, 115, 102, 105, 115, 104, 0];
 
 fn main() {
-    let a: usize = 42;
-    let b: &[u8; 10] = &B;
-    let c: Box<[u8]> = Box::new(C);
+    let a: i64 = 42;
+    let a_ptr = &a as *const i64;
+    let a_addr: usize = unsafe { std::mem::transmute(a_ptr) };
 
-    println!("a (an unsigned integer): ");
-    println!(" location: {:p}", &a);
-    println!(" size:     {:?} bytes", size_of::<usize>());
-    println!(" value:    {:?}", a);
-    println!();
+    println!("a: {} ({:p}..0x{:x})", a, a_ptr, a_addr + 7);
 
-    println!("b (a reference to B): ");
-    println!("  location:   {:p}", &b);
-    println!("  size:       {:?} bytes", size_of::<&[u8; 10]>());
-    println!("  points to:  {:p}", b);
-    println!();
+    let ptr = 42 as *const Vec<String>;
 
-    println!("c (a \"box\" for C):");
-    println!("  location:   {:p}", &c);
-    println!("  size:       {:?} bytes", size_of::<Box<[u8]>>());
-    println!("  points to:  {:p}", c);
-    println!();
-
-    println!("B (an array of 10 bytes)");
-    println!("  location:   {:p}", &B);
-    println!("  size:       {:?}", size_of::<[u8;10]>());
-    println!("  value:      {:?}", B);
+    unsafe {
+        let new_addr = ptr.offset(4);
+        println!("{:p} -> {:p}", ptr, new_addr);
+    }
 }
+
+// fn main() {
+//     let a = 42;
+//     let b: String;
+//     let c: Cow<str>;
+
+//     unsafe {
+//         let b_ptr = &B as *const u8 as *mut u8;
+//         b = String::from_raw_parts(b_ptr, 10, 10);
+
+//         let c_ptr = &C as *const u8 as *const c_char;
+
+//         c = CStr::from_ptr(c_ptr).to_string_lossy();
+//     }
+
+//     println!("a: {}, b: {}, c: {}", a, b, c);
+// }
